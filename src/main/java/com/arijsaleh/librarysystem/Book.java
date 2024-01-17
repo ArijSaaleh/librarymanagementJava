cf6a0602a097989274f7ffa0f0ae72aeb8f68f13
ArijSaaleh <arij.saleh@esprit.tn>
2024-01-17 19:29:23 +0100

    book class

---
+++ src/main/java/com/arijsaleh/librarysystem/Book.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.arijsaleh.librarysystem;
+
+/**
+ *
+ * @author arijs
+ */
+public class Book {
+
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    public Book(String title, String author, String isbn, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "title=" + title + ", author=" + author + ", isbn=" + isbn + ", available=" + available + '}';
+    }
+    
+}
